8daf018834b9390ffa711b5165dbe4ad5142c44e
nhatminhbeo <tqnmb437@gmail.com>
2017-02-10 19:01:48 -0800

    REST API

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ var http = require('http');
 var path = require('path');
 var bodyParser = require('body-parser');
 // var mongodb = require('./logic/connect');
+var mongodb = require('mongodb');
+var dburi = "mongodb://localhost:27017/example";
 
 // Server
 var express = require('express')
@@ -23,3 +25,31 @@ app.listen(port, function() {
 app.get('/', function(req, res) {
 	res.sendFile('index.html');
 })
+
+
+
+
+/*
+// RESTful API route for controllers to get data from the logic layer, and serve the view layer
+app.get('/data', function(req, res) {
+	// Connect to mongodb server
+	mongodb.MongoClient.connect(dburi, function(error, db) {
+		// Failed to connect
+		if (error) {
+			console.log(error);
+			process.exit(1);
+		}
+		// Find & print all entries
+		db.collection('sample').find().toArray(function(error, docs) {
+			// Failed to find (query) 
+			if (error) {
+				console.log(error);
+				
+			}
+
+			console.log('Found docs:');
+			res.status(200).json(JSON.stringify(docs));
+		});
+	});
+});
+*/
